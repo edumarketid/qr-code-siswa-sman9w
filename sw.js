b9991a428fad0904b25da2c456b9300b86f8e99e
edumarketid <edumarketid@gmail.com>
2026-09-22 20:05:57 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'absensi-app-cache-v2.25';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      const cachePromises = STATIC_ASSETS.map(url => {
+        return fetch(url).then(res => {
+          if (res.status === 200 || res.type === 'opaque') {
+            return cache.put(url, res);
+          }
+        }).catch(err => console.log('Fail caching: ', url));
+      });
+      return Promise.all(cachePromises);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('script.google.com')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request).then((networkResponse) => {
+        if (e.request.method === 'GET') {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
